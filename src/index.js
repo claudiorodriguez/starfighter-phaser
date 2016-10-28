204c7a53feb430c33ff69b28a33c5948a88c73d6
Claudio Rodriguez <cjrodr@yahoo.com>
2016-10-28 19:57:38 +0100

    feat: move and fire

--- src/index.js
+++ src/index.js
@@ -2,6 +2,68 @@ import 'pixi';
 import 'p2';
 import Phaser from 'phaser';
 import evilShip from './assets/evil-ship.png';
+import crossBullet from './assets/cross-bullet.png';
+
+const state = {};
+
+
+const preload = () => {
+  state.game.load.image('evilShip', evilShip);
+  state.game.load.image('crossBullet', crossBullet);
+};
+
+const create = () => {
+    const ship = state.game.add.sprite(400, 300, 'evilShip');
+
+    state.game.physics.arcade.enable(ship);
+
+    const weapon = state.game.add.weapon(30, 'crossBullet');
+    weapon.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS;
+    weapon.bulletSpeed = 400;
+    weapon.fireRate = 60;
+    weapon.bulletAngleVariance = 10;
+    weapon.trackSprite(ship, 91, 60);
+
+    const cursors = state.game.input.keyboard.createCursorKeys();
+    const fireButton = state.game.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
+
+    state.ship = ship;
+    state.weapon = weapon;
+    state.controls = {
+      cursors,
+      fireButton
+    };
+}
+
+const update = () => {
+  state.ship.body.velocity.x = 0;
+  state.ship.body.velocity.y = 0;
+
+  if (state.controls.cursors.left.isDown)
+  {
+      state.ship.body.velocity.x = -200;
+  }
+
+  else if (state.controls.cursors.right.isDown)
+  {
+      state.ship.body.velocity.x = 200;
+  }
+
+  if (state.controls.cursors.up.isDown)
+  {
+      state.ship.body.velocity.y = -200;
+  }
+
+  else if (state.controls.cursors.down.isDown)
+  {
+      state.ship.body.velocity.y = 200;
+  }
+
+  if (state.controls.fireButton.isDown)
+  {
+      state.weapon.fire();
+  }
+};
 
 const game = new Phaser.Game(
   800,
@@ -9,27 +71,10 @@ const game = new Phaser.Game(
   Phaser.AUTO,
   'content',
   {
-    preload: preload,
-    create: create
+    create,
+    preload,
+    update
   }
 );
 
-function preload() {
-
-    //  You can fill the preloader with as many assets as your game requires
-
-    //  Here we are loading an image. The first parameter is the unique
-    //  string by which we'll identify the image later in our code.
-
-    //  The second parameter is the URL of the image (relative)
-    game.load.image('einstein', evilShip);
-
-}
-
-function create() {
-
-    //  This creates a simple sprite that is using our loaded image and
-    //  displays it on-screen
-    game.add.sprite(0, 0, 'einstein');
-
-}
+state.game = game;
